c7fd079bdcd0fbf60bc1f167b300caf78ef3962e
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-30 13:48:48 +0900

    test: 회원 도메인 테스트

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,8 +1,22 @@
 package hello.core.member;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
 
     @Test //@Test쓰면 자동 임포트 된다.
+    void join(){
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+
+    }
 }
