1f76a79c0415cd26c0d4a0a52d24a782d1446443
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-12-01 14:32:32 +0900

    test: AppConfig 적용을 위한 테스트 코드 리팩토링

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,10 +1,18 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test //@Test쓰면 자동 임포트 된다.
     void join(){
