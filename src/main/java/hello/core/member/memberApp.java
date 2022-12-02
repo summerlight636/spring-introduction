95a24aeefcaf0f22cdbd4a20e8d1ef451138c732
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-12-02 17:56:27 +0900

    refactor(memberApp): ApplicationContext 사용하는 방법으로 변경
    기존: appConfig.memberService()
    변경: applicationContext.getBean("memberService", MemberService.class)

--- src/main/java/hello/core/member/memberApp.java
+++ src/main/java/hello/core/member/memberApp.java
@@ -1,11 +1,16 @@
 package hello.core.member;
 
 import hello.core.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class memberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);//ApplicationContext : 스프링의 DI컨테이너
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); //p1: 메서드 이름, p2: 메서드의 반환 타입 (AppConfig의)
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
