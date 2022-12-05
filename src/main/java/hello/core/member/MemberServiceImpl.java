d0e023e4ffaf343a05c6075e5df4c0b70604ee78
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-12-05 15:08:30 +0900

    refactor: @Component 와 @Autowired 적용

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,13 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component //Bean은 자동 등록되지만, 의존 관계 주입은 할 방법이 없음. 따라서 생성자 위에 @Autowired 를 해주어야 함.
 public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
 
+    @Autowired //ac.getBean(MemberRepository.class) 와 같이 동작
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
