90b3a72175c773eb842e3dd494cf46b889f3004f
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-12-06 15:40:41 +0900

    [8. 의존관계 자동 주입] 롬복 적용하기
    롬복 라이브러리를 적용하면 ㅇ
    1) @Setter, @Getter 만 적으면 세터와 게터가 생성 된다.
    2) @RequiredArgsConstructor 만 적으면 final 이 붙은 필드를 모아서 생성자를 자동으로 만들어 준다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,10 +6,12 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository; //생성자에 의해서만 값을 넣어줄 수 있음
     private final DiscountPolicy discountPolicy;
@@ -25,11 +27,11 @@ public class OrderServiceImpl implements OrderService{
 //        this.memberRepository = memberRepository;
 //    }
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired //생성자 하나 일 때는 생략가능
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    } //롬복의 @RequiredArgsConstructor 에 의해 자동 생성, 이미 만들어져 있는 취급 (Ctrl+F12 로 확인 가능) //자바의 애노테이션 프로세서 기능을 이용해서 컴파일 시점에 생성자 코드를 자동으로 생성해주는 것
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
