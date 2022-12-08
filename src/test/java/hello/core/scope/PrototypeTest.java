d3e5f9535660d84504e1dd2984997daaef60f547
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-12-08 14:06:39 +0900

    [10. 빈 스코프] 프로토 타입 스코프와 싱글톤 타입 스코프 비교
    - 프로토타입 빈의 생성과 의존관계 주입과 초기화까지만 관여, 따라서 @PreDestroy에 해당하는 종료 메서드가 호출되지 않는다
    - @Bean 또는 @Component 위에 @Scope("prototype")를 작성해서 스코프를 지정한다

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,40 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2 ");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close(); //close가 수행되는데도 @PreDestroy가 실행되지 않음
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("SingletonBean.close");
+        }
+    }
+}
